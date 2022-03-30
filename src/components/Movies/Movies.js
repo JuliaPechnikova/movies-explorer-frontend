06cf1aa2afa71157e5a6fcc083a282a20b875ca7
Julia Pechnikova <juliape4nikova@yandex.ru>
2022-03-31 02:19:58 +0300

    adding cards

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,6 +1,5 @@
 import SearchForm from '../SearchForm/SearchForm.js';
 import MoviesCardList from '../MoviesCardList/MoviesCardList.js';
-import {temp_all_movies} from '../../utils/const.js';
 
 function Movies(props) {
   return (
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,27 +1,42 @@
 import './MoviesCardList.css';
 import MoviesCard from '../MoviesCard/MoviesCard.js';
+import React from 'react';
 
 function MoviesCardList(props) {
-  const cards = props.cards;
-  let maxCards;
 
-  if (window.innerWidth > 1270) {
-    maxCards = 12;
-  }
-  else if (window.innerWidth < 1270 && window.innerWidth > 765) {
-    maxCards = 8;
-  }  
-  else if (window.innerWidth < 765) {
-    maxCards = 5;
+  const cards = props.cards;
+  const [maxCards, setMaxCards] = React.useState(0);
+  const [hideButton, setHideButton] = React.useState(false);
+
+  React.useEffect(() => {
+    if (window.innerWidth > 1270) {
+      setMaxCards(12);
+    }
+    else if (window.innerWidth < 1270 && window.innerWidth > 765) {
+      setMaxCards(8)  
+    }  
+    else if (window.innerWidth < 765) {
+      setMaxCards(5)  
+    }
+  }, [window.innerWidth]);
+
+  function handleClickButton () {
+    if (window.innerWidth > 1270) {
+      setMaxCards(maxCards + 3);
+    }
+    else if (window.innerWidth < 1270) {
+      setMaxCards(maxCards + 2);
+    }
   }
 
   return (
     <section className="movies-card-list">
       <ul className={`movies-card-list__elements ${props.savedButton ? "movies-card-list__elements_saved-movies" : ""}`}> 
-          {cards.map((card, index) => (
-            (index < maxCards) ? <MoviesCard card={card} key={card.id} savedButton={props.savedButton}/> : <></>))
-}     </ul>
-      <button className={`movies-card-list__button ${props.savedButton ? "movies-card-list__button_saved-movies" : ""}`}>Еще</button>
+          {cards.length === 0 ? <p>Ничего не найдено</p> :
+            cards.map((card, index) => ((index < maxCards) ? <MoviesCard card={card} key={card.id} savedButton={props.savedButton}/> : <></>))
+          }
+      </ul>
+      {cards.length === 0 || props.savedButton === true ? <></> : <button className="movies-card-list__button" onClick={handleClickButton}>Еще</button>}
     </section>
   ); 
 }
