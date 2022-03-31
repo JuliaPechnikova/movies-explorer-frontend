e7ea86cbffa676c3d6a76abd14c3868d561063e9
Julia Pechnikova <juliape4nikova@yandex.ru>
2022-04-01 00:10:50 +0300

    added display card dependency on screen size

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -7,24 +7,59 @@ function MoviesCardList(props) {
   const cards = props.cards;
   const [maxCards, setMaxCards] = React.useState(0);
   const [hideButton, setHideButton] = React.useState(false);
+  const [windowSize, setWindowSize] = React.useState(undefined);
+
+  const getWidth = () => window.innerWidth 
+    || document.documentElement.clientWidth 
+    || document.body.clientWidth;
+
+  function useCurrentWidth() {
+    // save current window width in the state object
+    let [width, setWidth] = React.useState(getWidth());
+
+    // in this case useEffect will execute only once because
+    // it does not have any dependencies.
+    React.useEffect(() => {
+      // timeoutId for debounce mechanism
+      let timeoutId = null;
+      const resizeListener = () => {
+        // prevent execution of previous setTimeout
+        clearTimeout(timeoutId);
+        // change width from the state object after 150 milliseconds
+        timeoutId = setTimeout(() => setWidth(getWidth()), 150);
+      };
+      // set resize listener
+      window.addEventListener('resize', resizeListener);
+
+      // clean up function
+      return () => {
+        // remove resize listener
+        window.removeEventListener('resize', resizeListener);
+      }
+    }, [])
+
+    return width;
+  }
+
+  let width = useCurrentWidth();
 
   React.useEffect(() => {
-    if (window.innerWidth > 1270) {
+    if (width > 1270) {
       setMaxCards(12);
     }
-    else if (window.innerWidth < 1270 && window.innerWidth > 765) {
+    else if (width < 1270 && width > 765) {
       setMaxCards(8)  
     }  
-    else if (window.innerWidth < 765) {
+    else if (width < 765) {
       setMaxCards(5)  
     }
-  }, [window.innerWidth]);
+  }, [width]);
 
   function handleClickButton () {
-    if (window.innerWidth > 1270) {
+    if (width > 1270) {
       setMaxCards(maxCards + 3);
     }
-    else if (window.innerWidth < 1270) {
+    else if (width < 1270) {
       setMaxCards(maxCards + 2);
     }
   }
@@ -36,7 +71,7 @@ function MoviesCardList(props) {
             cards.map((card, index) => ((index < maxCards) ? <MoviesCard card={card} key={card.id} savedButton={props.savedButton}/> : <></>))
           }
       </ul>
-      {cards.length === 0 || props.savedButton === true ? <></> : <button className="movies-card-list__button" onClick={handleClickButton}>Еще</button>}
+      {cards.length > 0 && cards.length > maxCards ? <button className="movies-card-list__button" onClick={handleClickButton}>Еще</button> : <></>}
     </section>
   ); 
 }
